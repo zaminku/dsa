5ec6ae9953866a00d3887d411ab3c25f98e60329
zaminku <zaminkugshia@gmail.com>
2022-01-25 18:11:15 -0600

    get node value

--- linked_list.js
+++ linked_list.js
@@ -27,3 +27,22 @@ const linkedListFind = (head, target) => {
     }
     return false;
 };
+
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+const getNodeValue = (head, index) => {
+    let count = 0;
+    let current = head;
+    while (current !== null) {
+        if (count === index) return current.val;
+        current = current.next;
+        count += 1;
+    }
+    return null;
+};
+
