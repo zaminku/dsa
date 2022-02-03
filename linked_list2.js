2b6085e7474148acb12e4acea795fc26f0baadc1
zaminku <zaminkugshia@gmail.com>
2022-02-02 18:39:37 -0600

    complete reverse LL with recursive solution

--- linked_list2.js
+++ linked_list2.js
@@ -107,3 +107,76 @@ const linkedListFind = (head, target) => {
 // recursive
 // time: O(n)
 // space: O(n) - bc of the call stack
+
+
+
+
+// Get node value
+
+const getNodeValue = (head, index) => {
+    // todo
+    //   let currIndex = 0;
+    //   let currNode = head;
+
+    //   while (currNode !== null) {
+    //     if (currIndex === index) {
+    //       return currNode.val;
+    //     }
+    //     currIndex += 1;
+    //     currNode = currNode.next;
+    //   }
+
+    //   return null;
+
+    // recursive
+    // if (head === null) return null;
+    // if (index === 0) return head.val;
+    // return getNodeValue(head.next, index - 1);
+};
+
+// iterative
+// time: O(n)
+// space: O(1)
+
+// recursive:
+// time: O(n)
+// space: O(n) // storing every call on the call stack
+
+
+
+const reverseList = (head) => {
+  // todo
+  let current = head;
+  let prev = null; // prev var for current.next = prev;
+  
+  while (current !== null) {
+    let next = current.next; // need to keep track of what the initial next is
+    current.next = prev; // reverse the order
+    prev = current; // update prev to current node
+    current = next; // update current to next node
+  }
+  
+  return prev; // current === null, so prev will be the new head of the node (last non-null node)
+
+}
+
+
+// iterative
+// time: O(n)
+// space: O(1)
+
+// const reverseList = (head, prev = null) => {
+//   // todo
+//   if (head === null) return prev;
+//   const next = head.next;
+//   head.next = prev;
+  
+//   return reverseList(next, head);
+  
+
+// }
+
+
+// recursive:
+// time: O(n)
+// space: O(n)
